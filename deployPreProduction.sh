bdbfc7422c9bfbca69e89e00f22b3c16a7f14ba5
Stephan Schwab <sns@caimito.net>
2018-05-14 17:03:23 +0300

    preprod

--- deployPreProduction.sh
+++ deployPreProduction.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-create vagrant VM via Ansible with Docker (same playbook as for production)
-acquire testdate from production
-deploy containers
-run app
+# vagrant destroy -f
+vagrant provision
+if [ $? -ne 0 ]; then
+    exit
+fi
+
+# acquire testdata from production
+
+vagrant scp docker-compose-preprod.yml :animals.yml
+  if [ $? -ne 0 ]; then
+    exit
+  fi
+
+vagrant ssh -c "sudo docker pull snscaimito/animals"
+  if [ $? -ne 0 ]; then
+    exit
+  fi
+vagrant ssh -c "sudo docker-compose -f animals.yml up -d"
