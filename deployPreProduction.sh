e0e552f99da3684759e1dc0f3b170067fda99c64
Stephan Schwab <sns@caimito.net>
2018-05-15 19:48:26 +0300

    set up pre prod env

--- deployPreProduction.sh
+++ deployPreProduction.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-# vagrant destroy -f
-vagrant provision
+vagrant destroy -f
+vagrant up
+#vagrant provision
 if [ $? -ne 0 ]; then
     exit
 fi
 
-# acquire testdata from production
+ssh bosque.caimito.net docker exec sns_mongo_1 mongodump --out /backup
+scp -r bosque.caimito.net:backup target/
+vagrant ssh -c "mkdir -p backup"
+vagrant scp target/backup :.
 
 vagrant scp docker-compose-preprod.yml :animals.yml
   if [ $? -ne 0 ]; then
@@ -18,3 +22,5 @@ vagrant ssh -c "sudo docker pull snscaimito/animals"
     exit
   fi
 vagrant ssh -c "sudo docker-compose -f animals.yml up -d"
+
+vagrant ssh -c "docker exec vagrant_mongo_1 mongorestore /backup"
--- deployProduction.sh
+++ deployProduction.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ssh bosque.caimito.net mkdir -p backup
 scp docker-compose.yml bosque.caimito.net:animals.yml
 ssh bosque.caimito.net docker pull snscaimito/animals
 ssh bosque.caimito.net docker-compose -f animals.yml up -d
